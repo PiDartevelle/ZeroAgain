2e1e120d936c450e9f2392df21944b4c8fc1e16d
JBillault <billaultjonathan@gmail.com>
2022-03-22 15:43:56 +0100

    New update for the game html file and css file.

--- tertrisNew.js
+++ tertrisNew.js
@@ -1,12 +1,15 @@
 // ---------- DECLARATION DES VARIABLES ---------- //
 
 const gameBoard = document.getElementById("gameBoard");
-const context = gameBoard.getContext('2d');
+const canvas = document.createElement('canvas');
+let largeurCanvas = gameBoard.clientWidth; // récupère la largeur en pixel de la div gameBoard
+let hauteurCanvas = gameBoard.clientHeight; // récupère la hauteur en pixel de la div gameBoard
 
 let delay = 300; // délai de rafraichissement de la fonction refreshCanvas
 const largeurGrille = 14; // Nombre de cases en largeur
 const hauteurGrille = 28; // Nombre de cases en hauteur
-const carreaux = 20; //Taille en pixel d'une case de la grille
+// const carreaux = largeurCanvas / largeurGrille; //Taille en pixel d'une case de la grille
+const carreaux = hauteurCanvas / hauteurGrille;
 
 const xInitial = 5; // Position de départ de la forme sur la grille
 const yInitial = 0; // Position de départ de la forme sur la grille
@@ -121,10 +124,8 @@ forme[4] = [  // Forme 5
 
 forme[5] = [  // Forme 6
    [
-        [0,0,0,0],
-        [0,1,1,0],
-        [0,1,1,0],
-        [0,0,0,0]
+        [1,1],
+        [1,1],
    ]
 ];
 
@@ -143,15 +144,17 @@ forme[6] = [  // Forme 7
   ],
 ]; 
 
+const grille = new Array(hauteurGrille);
+
 // ---------- FONCTIONS ---------- //
 
 // Fonction de dessin des formes. 
 // Seules les cases ou il y a un 1 dans le tableau des formes sont coloriées.
 function drawForme() {
-    for(let i = 0; i < forme[numForme][rotation].length;i++){
+    for(let i = 0; i < forme[numForm][rotation].length;i++){
         for(let j = 0; j < forme[numForm][rotation].length; j++){
             if(forme[numForm][rotation][j][i] == 1){
-                context.fillStyle = couleursFormes[1][numForme]; // Couleur de contour de la forme
+                context.fillStyle = couleursFormes[1][numForm]; // Couleur de contour de la forme
                 context.fillRect((xForm + i) * carreaux, (yForm + j) * carreaux, carreaux, carreaux); // fillRect(coordonnée x, coordonnée y, largeur, hauteur)
                 context.fillStyle = couleursFormes[0][numForm]; // Couleur de remplissage de la forme
                 context.fillRect((xForm + i) * carreaux + 1, (yForm + j) * carreaux + 1, carreaux - 2, carreaux - 2);
@@ -164,34 +167,145 @@ function drawForme() {
 // Efface le canvas et dessine la forme
 function refreshCanvas(){
     context.clearRect(0, 0, largeurGrille * carreaux, hauteurGrille * carreaux); // met en noir transparent tous les pixels dans le rectangle définis en supprimant tout ce qui a été dessiné avant. 
-    //v clearRect(coordonnée x du point de départ du rect, coordonnée y du point de départ du rect, largeur, hauteur)
+    // clearRect(coordonnée x du point de départ du rect, coordonnée y du point de départ du rect, largeur, hauteur)
     drawForme();
     yForm++;
-    if(yForm > hauteurGrille){
-        yForm = 0;
+    //console.log(yForm);
+    //console.log(collision());
+       /*if(yForm > hauteurGrille){
+       yForm = 0;
+    }*/
+    if(collision()){
+        yForm--;
+        copieFormeDansLaGrille();
+        drawForme();
+        console.table(grille);
+        yForm = yInitial;
+        xForm = xInitial;
+        rotation = 0;
     }
 };
 
 // Fonction de gestion des collisions
-  /* function collision(){
-        for(let i = 0; i < forme[numForme][rotation].length; i++){
-            for(let j = 0; i < forme[numForme][rotation].length; j++){
-                if(forme[numForme][rotation][j][i] == 1){
-                    if(formX > 0 && formX < LARGEUR_GRILLE){
+function collision(){
+        for(let i = 0; i < forme[numForm][rotation].length; i++){
+            for(let j = 0; j < forme[numForm][rotation].length; j++){
+                if(forme[numForm][rotation][j][i] == 1){ // Pour toutes les cases des formes qui sont égales à 1 
+                    if(xForm < 0 || xForm + forme[numForm][rotation].length > largeurGrille){ // on autorise les mouvements de gauche à droite 
                         return true;
                     }
-                    if(formY > HAUTEUR_GRILLE){
-                        return true
+                    if(yForm + forme[numForm][rotation].length > hauteurGrille){ // on autorise la decente de la forme jusqu'au bas de la grille
+                        return true;
+                    } 
+                    else{
+                        return false;
                     }
-                    return false;
+                    return false; // dnas les autres cas, le mouvement est impossible
                 }
             }
         }
 
-    }*/
+    };
 
+// Fonction d'initialisation de la grille
+function initGrille(){
+    for(let i = 0; i < grille.length; i++){
+        grille[i] = new Array(largeurGrille);
+    }
+    for(let j = 0; j < grille.length; j++){
+        for(let k = 0; k < grille[j].length; k++){
+            grille[j][k] = -1;
+        }
+    }
+};
+
+// Fonction de copie de la forme dans la grille
+function copieFormeDansLaGrille(){
+    for(let i = 0; i < forme[numForm][rotation].length; i++){
+        for(let j = 0; j < forme[numForm][rotation].length; j++){
+            if(forme[numForm][rotation][j][i] == 1){
+                grille[yForm][xForm] = numForm;
+            }
+        }
+    }
+};
+
+// Fonction pour afficher la grille
+function drawGrille(){
+    for(let i = 0; i < grille.length; i++){
+        for(let j = 0; j < grille[i].length; j++){
+            if(grille[i][j] > -1){
+                context.fillStyle = couleursFormes[1][numForm]; // Couleur de contour de la forme
+                context.fillRect((xForm + i) * carreaux, (yForm + j) * carreaux, carreaux, carreaux); // fillRect(coordonnée x, coordonnée y, largeur, hauteur)
+                context.fillStyle = couleursFormes[0][numForm]; // Couleur de remplissage de la forme
+                context.fillRect((xForm + i) * carreaux + 1, (yForm + j) * carreaux + 1, carreaux - 2, carreaux - 2);
+            }
+        }
+    }
+}
 // ---------- CODE ---------- //
 
-gameBoard.width = largeurGrille * carreaux;
-gameBoard.height = hauteurGrille * carreaux;
-gameBoard.style.border = "1px solid";
+// initialisation du canvas
+canvas.width = largeurGrille * carreaux;
+canvas.height = hauteurGrille * carreaux;
+canvas.style.border = "1px solid";
+gameBoard.appendChild(canvas);
+const context = canvas.getContext('2d');
+refreshCanvas();
+setInterval(refreshCanvas, delay);
+initGrille();
+
+// Gestion des évènements clavier
+window.addEventListener("keydown", function(event){
+    let key = event.key;
+    let temporaire = 0; // Pour mémoriser temorairement la rotation 
+    switch(key){
+        case 'ArrowUp':
+            temporaire = rotation;
+            rotation++;
+            if(rotation > forme[numForm].length - 1){
+                rotation = 0;
+            }
+            if(collision()){
+                rotation = temporaire;
+            }
+            break;
+        case 'ArrowDown':
+            temporaire = rotation;
+            rotation--;
+            if(rotation < 0){
+                rotation = forme[numForm].length - 1;
+            }
+            if(collision()){
+                rotation = temporaire;
+            }
+            break;
+        case 't':
+            rotation = 0;
+            numForm++;
+            if(numForm > forme.length - 1){
+                numForm = 0;
+            }
+            break;
+        case 'ArrowLeft':
+            temporaire = xForm;
+            if(xForm > 0){
+                xForm--;
+            }
+            if(collision()){
+                xForm = temporaire;
+            }
+            break;
+        case 'ArrowRight':
+            temporaire = xForm;
+            if(xForm < largeurGrille - 3){
+                xForm++;
+            }
+            if(collision()){
+                xForm = temporaire;
+            }
+            break;
+    }
+});
+
+
