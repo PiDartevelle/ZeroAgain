1246850a991e8cc1b8194e3c9d32a4b03d02de1c
ChrichriR <chriss_s@hotmail.fr>
2022-03-23 17:07:03 +0100

    annulation of the sounds features and modifications of the rules

--- settings.js
+++ settings.js
@@ -11,3 +11,10 @@ const buttonDiff = document.querySelector(".diff");
 buttonDiff.addEventListener("click", () => {
   chooseMode.classList.toggle("pop");
 });
+
+const btn = document.getElementById("rules");
+const rulesNeon = document.querySelector(".neon");
+
+btn.addEventListener("click", () => {
+  rulesNeon.classList.toggle("style");
+});
