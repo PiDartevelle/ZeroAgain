6e4a9df2572d0b711e482f957441bac554f867ea
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-03-24 11:09:39 +0100

    fixed indentation

--- about-us.js
+++ about-us.js
@@ -11,19 +11,8 @@ submitBtn.addEventListener('click', (e)=>{
     e.preventDefault();
     console.log(userFirstName.value, userMail.value);
     alert('Thank you for taking the time, we will be in touch!');
-   
-   
-   
-   
-   
     userFirstName.value = "";
     userLastName.value = "";
     userMail.value = "";
     userMessage.value = "";
-  
-  
-   
-
 });
-
-localStorage.setItem()
