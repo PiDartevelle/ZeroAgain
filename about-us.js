940df51611b8658b2e8e724d1c54d06f15117e42
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-03-24 09:54:06 +0100

    added about-us.js, alert and reseted values upon click

---
+++ about-us.js
@@ -0,0 +1,20 @@
+// giving the feedback button some life //
+
+
+const submitBtn = document.getElementById('submit-btn');
+const userFirstName = document.getElementById('userFirstName');
+const userLastName = document.getElementById('userLastName');
+const userMail = document.getElementById('userMail');
+const userMessage = document.getElementById('sendMessage');
+
+submitBtn.addEventListener('click', (e)=>{
+    alert('Thank you for taking the time, we will be in touch!');
+    userFirstName.value = "";
+    userLastName.value = "";
+    userMail.value = "";
+    userMessage.value = "";
+  
+  
+    //  console.log('coucoucoucoucoucoucou')
+
+});
