6ddec264d56fb4c24ba0c0c0d049aa4d1071d9d5
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-03-24 10:42:55 +0100

    added an aboutus.js file

---
+++ about-us.js
@@ -0,0 +1,17 @@
+// giving the feedback button some life //
+
+
+const submitBtn = document.getElementById('submit-btn');
+const userFirstName = document.getElementById('userFirstName');
+const userLastName = document.getElementById('userLastName');
+const userMail = document.getElementById('userMail');
+const userMessage = document.getElementById('sendMessage');
+
+submitBtn.addEventListener('click', (e)=>{
+    console.log(userFirstName.value, userMail.value);
+    alert('Thank you for taking the time, we will be in touch!');
+    userFirstName.value = "";
+    userLastName.value = "";
+    userMail.value = "";
+    userMessage.value = "";
+});
